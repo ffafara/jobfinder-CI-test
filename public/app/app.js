9af7e6220a6e932fc8b6e6499a8c010c0b8c66ee
Filip Fafara <ffafara@thoughtworks.com>
2015-08-10 17:27:19 -0400

    submit form and karma for front-end testing

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,10 @@
 var app = angular.module('app', ['ngResource'])
 	.controller('testCtrl', function($scope, $resource, jobs){
 	$scope.jobs = $resource('/api/jobs').query();
-	jobs.save({title: "test title", description: "test description"});
+	$scope.submit = function() {
+		var job = {title: $scope.title, description: $scope.description}
+		jobs.save(job);
+		$scope.jobs.push(job);	
+	}
+	
 });
